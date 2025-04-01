fc300a6073d13573ac49a39951467cb9f69c2a42
sergei_arkhipov_1 <byrgyin@yandex.ru>
2025-04-01 17:45:44 +0300

    faulre_pug

--- config_builder/plugins.js
+++ config_builder/plugins.js
@@ -3,6 +3,9 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "node:path";
 
 const __dirname = path.resolve();
+console.log('LOOK AT ME')
+console.log(__dirname);
+console.log('LOOK AT ME')
 
 /*-- CSS CONFIG  --*/
 export const cssMiniCssExtract = new MiniCssExtractPlugin({
@@ -12,7 +15,7 @@ export const cssMiniCssExtract = new MiniCssExtractPlugin({
 
 /*-- PUG-TO-HTML CONFIG  --*/
 export const pluginGenerateHtml = new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src/index.pug'),
+    template: path.resolve(__dirname, './src/pug/pages/index.pug'),
     filename: 'index.html',
     minify: false,
     inject: true,
